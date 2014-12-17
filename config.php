345c83f1d561457177dd7db4164eda596c409c96
mauu1988 <mauriziogargiulo6@gmail.com>
2014-12-17 22:16:48 +0100

    config.php

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+ 
+ 
+$email_admin = "mauriziogargiulo6@gmail.com";
+ 
+$sito_internet  = "http://mauu1988.altervista.org/";
+ 
+$grazie = "http://mauu1988.altervista.org/index.php";
+//Pagina di redirect
+
+$host = "localhost";
+
+$user_db = "root";
+
+$password_db = "mau*1*2*3";
+ 
+?>
